f252841b6ddd5c1ab2d8d88b136a8ca3f91e2a1b
Jovial Joe Jayarson <jovial7joe@hotmail.com>
2021-08-23 15:30:21 +0530

    feat: updates installed apps and routes

--- cbcefdb/settings.py
+++ cbcefdb/settings.py
@@ -31,15 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'form.apps.FormConfig',
-    "crispy_forms",
-    "crispy_bootstrap5",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "crispy_forms",
+    "crispy_bootstrap5",
+    "survey.apps.SurveyConfig"
 ]
 
 MIDDLEWARE = [
