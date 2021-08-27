f2b380c675cc419101cba5034f1e9da452bfb44d
Jovial Joe Jayarson <jovial7joe@hotmail.com>
2021-08-27 20:31:10 +0530

    feat: adds fam_info; fix: renames urls

--- survey/urls.py
+++ survey/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from . import views
+from .views import HomeView, FamilyView, FamilyCreateView, FamilyInfoView
 
-app_name = "survey"
+app_name = "survey"  # avoids creating template/app_name
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name="survey-home"),
-    path('families/', views.FamilyView.as_view(), name="list_family"),
-    path('families/add', views.FamilyCreateView.as_view(), name="add_family"),
-
+    path('', HomeView.as_view(), name="survey_home"),
+    path('families/', FamilyView.as_view(), name="family_list"),
+    path('families/add/', FamilyCreateView.as_view(), name="family_add"),
+    path('families/<int:pk>/', FamilyInfoView.as_view(), name='family_info'),
 ]
